4f2e7dd84a4728043e498c315b3912e835524de0
Aaron Nolan <N11021276@qut.edu.au>
2021-11-19 15:18:52 +1000

    Profile page completed
    - Users can edit their username, email and profile picture

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import User
@@ -22,7 +24,7 @@ class RegistrationForm(FlaskForm):
 
         if username_taken:
             raise ValidationError(
-                'That username has been taken. Please choose a different one.')
+                'That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
 
@@ -40,3 +42,35 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+
+class UpdateProfileForm(FlaskForm):
+
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Save changes')
+
+    def validate_username(self, username):
+
+        if username.data != current_user.username:
+
+            username_taken = User.query.filter_by(
+                username=username.data.lower()).first()
+
+            if username_taken:
+                raise ValidationError(
+                    'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+
+        if email.data != current_user.email:
+
+            email_taken = User.query.filter_by(
+                email=email.data.lower()).first()
+
+            if email_taken:
+                raise ValidationError(
+                    'An account already exists using this email. Please choose a different one.')
--- application/profile.py
+++ application/profile.py
@@ -1,10 +1,65 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+import os
+import secrets
+from PIL import Image
+from flask import Blueprint, render_template, url_for, request
+from flask.helpers import flash
+from flask_login import login_required, current_user
+from werkzeug.utils import redirect
+from application.forms import UpdateProfileForm
+from application import db
 
 blueprint = Blueprint('profile', __name__, url_prefix='/profile')
 
 
-@blueprint.route("/")
+def save_picture(form_picture):
+
+    # Generate random file name for the image and concatenate with the file extension (jpg or png)
+    random_hex = secrets.token_hex(8)
+    _, file_extension = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_extension
+
+    # Create the image path
+    BASE_PATH = os.path.dirname(__file__)
+    picture_path = os.path.join(
+        BASE_PATH, 'static/profile_pics', picture_filename)
+
+    # Resize image
+    image_size = (400, 400)
+    image = Image.open(form_picture)
+    image.thumbnail(image_size)
+
+    # Save the image path
+    image.save(picture_path)
+
+    return picture_filename
+
+
+@blueprint.route("/", methods=['GET', 'POST'])
 @login_required
 def profile():
-    return render_template('profile.html')
+
+    form = UpdateProfileForm()
+
+    if form.validate_on_submit():
+
+        if form.picture.data:
+
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f"Your profile has been updated!", 'success')
+
+        return redirect(url_for('profile.profile'))
+
+    elif request.method == 'GET':
+
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for(
+        'static', filename=f'profile_pics/{current_user.image_file}')
+
+    return render_template('profile.html', title='Profile', image_file=image_file, form=form)
